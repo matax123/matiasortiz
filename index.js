5a4a24f5d409e9b876e19fe45740ac4e1a5a9547
matax123 <matias.ortiz@alumnos.ucn.cl>
2022-04-27 14:16:13 -0400

    hotfix

--- index.js
+++ index.js
@@ -6,33 +6,18 @@ color4 = "rgb(238, 238, 238)"
 new WOW().init()
 
 var imgCount;
-
-document.addEventListener("DOMContentLoaded", function () {
-    var imgs = document.querySelectorAll("img")
-    imgCount = imgs.length
-
-    for (var i = 0; i < imgs.length; i++) {
-        imgs[i].onload = function () {
-            imgLoaded()
-        }
-    }
-})
-
-async function imgLoaded(){
-    if(loadedCounter){
-        loadedCounter++
-    }
-    else{
-        var loadedCounter = 1
-    }
+var loadedCounter = 0
+function imgLoaded() {
+    loadedCounter++
     if (loadedCounter == imgCount) {
+        console.log("all images loaded")
         $("#loading").fadeOut(1000);
         $("body").removeClass("preload");
         var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-    
+
         var mobileURL = "https://api.whatsapp.com/send/?phone=56991353540&text=";
         var pcURL = "https://web.whatsapp.com/send?phone=56991353540&text=";
-    
+
         if (isMobile) {
             document.getElementById("telefono").href = mobileURL;
         } else {
@@ -40,3 +25,17 @@ async function imgLoaded(){
         }
     }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    var imgs = document.querySelectorAll(".projectImage")
+    console.log(imgs)
+    imgCount = imgs.length
+
+    for (var i = 0; i < imgs.length; i++) {
+        console.log("new img onload")
+        imgs[i].onload = function (e) {
+            console.log(e.target)
+            imgLoaded()
+        }
+    }
+})

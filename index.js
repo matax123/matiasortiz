78c86bb562aed79288766a9c03fd0c888b638183
matax123 <matias.ortiz@alumnos.ucn.cl>
2022-04-27 14:03:12 -0400

    fix: load time

--- index.js
+++ index.js
@@ -5,41 +5,38 @@ color4 = "rgb(238, 238, 238)"
 
 new WOW().init()
 
-window.onload = function() {
-    $("#loading").fadeOut(1000);
-    $("body").removeClass("preload");
-    var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+var imgCount;
 
-    var mobileURL = "https://api.whatsapp.com/send/?phone=56991353540&text=";
-    var pcURL = "https://web.whatsapp.com/send?phone=56991353540&text=";
+document.addEventListener("DOMContentLoaded", function () {
+    var imgs = document.querySelectorAll("img")
+    imgCount = imgs.length
 
-    if (isMobile) {
-        document.getElementById("telefono").href = mobileURL;
-    } else {
-        document.getElementById("telefono").href = pcURL;
+    for (var i = 0; i < imgs.length; i++) {
+        imgs[i].onload = function () {
+            imgLoaded()
+        }
+    }
+})
+
+async function imgLoaded(){
+    if(loadedCounter){
+        loadedCounter++
+    }
+    else{
+        var loadedCounter = 1
+    }
+    if (loadedCounter == imgCount) {
+        $("#loading").fadeOut(1000);
+        $("body").removeClass("preload");
+        var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    
+        var mobileURL = "https://api.whatsapp.com/send/?phone=56991353540&text=";
+        var pcURL = "https://web.whatsapp.com/send?phone=56991353540&text=";
+    
+        if (isMobile) {
+            document.getElementById("telefono").href = mobileURL;
+        } else {
+            document.getElementById("telefono").href = pcURL;
+        }
     }
 }
-// document.addEventListener("DOMContentLoaded", function () {
-
-//     var imgs = document.querySelectorAll("img")
-//     for (var i = 0; i < imgs.length; i++) {
-//         imgs[i].addEventListener("mouseover", function () {
-//             this.style.opacity = "0.5"
-//         })
-//         imgs[i].addEventListener("mouseout", function () {
-//             this.style.opacity = "1"
-//         })
-//     }
-//     $("#loading").fadeOut(1000);
-//     $("body").removeClass("preload");
-//     var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-
-//     var mobileURL = "https://api.whatsapp.com/send/?phone=56991353540&text=";
-//     var pcURL = "https://web.whatsapp.com/send?phone=56991353540&text=";
-
-//     if (isMobile) {
-//         document.getElementById("telefono").href = mobileURL;
-//     } else {
-//         document.getElementById("telefono").href = pcURL;
-//     }
-// })

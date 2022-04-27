d679974a2f294da8a97ad04308468ca6062a69a6
matax123 <matias.ortiz@alumnos.ucn.cl>
2022-04-27 13:51:49 -0400

    feat: emplesoschile

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ color4 = "rgb(238, 238, 238)"
 
 new WOW().init()
 
-document.addEventListener("DOMContentLoaded", function () {
+window.onload(function(){
     $("#loading").fadeOut(1000);
     $("body").removeClass("preload");
     var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
@@ -19,3 +19,27 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("telefono").href = pcURL;
     }
 })
+// document.addEventListener("DOMContentLoaded", function () {
+
+//     var imgs = document.querySelectorAll("img")
+//     for (var i = 0; i < imgs.length; i++) {
+//         imgs[i].addEventListener("mouseover", function () {
+//             this.style.opacity = "0.5"
+//         })
+//         imgs[i].addEventListener("mouseout", function () {
+//             this.style.opacity = "1"
+//         })
+//     }
+//     $("#loading").fadeOut(1000);
+//     $("body").removeClass("preload");
+//     var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+//     var mobileURL = "https://api.whatsapp.com/send/?phone=56991353540&text=";
+//     var pcURL = "https://web.whatsapp.com/send?phone=56991353540&text=";
+
+//     if (isMobile) {
+//         document.getElementById("telefono").href = mobileURL;
+//     } else {
+//         document.getElementById("telefono").href = pcURL;
+//     }
+// })

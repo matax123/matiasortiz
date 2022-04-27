b82190d233be477cef4ee4967d90b8bc07da44ad
matax123 <matias.ortiz@alumnos.ucn.cl>
2022-04-27 14:34:04 -0400

    hotfix

--- index.js
+++ index.js
@@ -5,37 +5,48 @@ color4 = "rgb(238, 238, 238)"
 
 new WOW().init()
 
-var imgCount;
-var loadedCounter = 0
-function imgLoaded() {
-    loadedCounter++
-    if (loadedCounter == imgCount) {
-        console.log("all images loaded")
-        $("#loading").fadeOut(1000);
-        $("body").removeClass("preload");
-        var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+// var imgCount;
+// var loadedCounter = 0
+// function imgLoaded() {
+//     loadedCounter++
+//     if (loadedCounter == imgCount) {
 
-        var mobileURL = "https://api.whatsapp.com/send/?phone=56991353540&text=";
-        var pcURL = "https://web.whatsapp.com/send?phone=56991353540&text=";
+//     }
+// }
 
-        if (isMobile) {
-            document.getElementById("telefono").href = mobileURL;
-        } else {
-            document.getElementById("telefono").href = pcURL;
-        }
+// document.addEventListener("DOMContentLoaded", function () {
+//     let imgs = document.querySelectorAll(".projectImage")
+//     console.log(imgs)
+//     imgCount = imgs.length
+
+//     for (var i = 0; i < imgs.length; i++) {
+//         console.log("new img onload")
+//         imgs[i].onload = function (e) {
+//             console.log(e.target)
+//             imgLoaded()
+//         }
+//     }
+// })
+
+window.onload = function(){
+    $("#loading").fadeOut(1000);
+    $("body").removeClass("preload");
+    var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+    var mobileURL = "https://api.whatsapp.com/send/?phone=56991353540&text=";
+    var pcURL = "https://web.whatsapp.com/send?phone=56991353540&text=";
+
+    if (isMobile) {
+        document.getElementById("telefono").href = mobileURL;
+    } else {
+        document.getElementById("telefono").href = pcURL;
     }
+
+    //load videos
+    let videoCatDisaster = document.getElementById("videoCatDisaster")
+    videoCatDisaster.src = "videos/videoCatDisaster.mp4"
+    let videoFT = document.getElementById("videoFT")
+    videoFT.src = "videos/Sistema FT.mp4"
+    let videoAntsWorld = document.getElementById("videoAntsWorld")
+    videoAntsWorld.src = "videos/antsWorld.mp4"
 }
-
-document.addEventListener("DOMContentLoaded", function () {
-    var imgs = document.querySelectorAll(".projectImage")
-    console.log(imgs)
-    imgCount = imgs.length
-
-    for (var i = 0; i < imgs.length; i++) {
-        console.log("new img onload")
-        imgs[i].onload = function (e) {
-            console.log(e.target)
-            imgLoaded()
-        }
-    }
-})
